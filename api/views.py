cd589214556c26ff3d0312bc9a5a2510cc5d788e
Avinash <avinashsri1605@gmail.com>
2023-11-23 19:36:06 +0530

    firs t commit

---
+++ api/views.py
@@ -0,0 +1,128 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from .models import *
+from rest_framework.decorators import api_view
+import nltk
+# nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+import string
+from nltk.corpus import stopwords
+from .models import *
+# nltk.download('punkt')
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import pandas as pd
+import numpy as np
+
+from .serializers import *
+#------------------------------------------------------------------------
+ # Load the saved model
+# embed = tf.saved_model.load(module_path)
+# sentence_encoder_layer=hub.KerasLayer(
+# "https://tfhub.dev/google/universal-sentence-encoder/4",
+# input_shape=[],
+# dtype=tf.string,
+# name="USE"
+# )
+
+custom_objects = {'KerasLayer': hub.KerasLayer}
+model = tf.keras.models.load_model("E:\ML\model_2(72% ts).h5", custom_objects=custom_objects)
+
+CLASSES=['POCSO-2012', 'IPC-509', 'IPC-504', 'IPC-354', 'IPC-354A', 'IPC-498A',
+       'IPC-302', 'IPC-376', 'DPA', 'DVA', 'IPC-34', 'IPC 307', 'IPC 313',
+       'IPC 109', 'IPC 324', 'IPC 326', 'IPC 323']
+#--------------------------------------------------------------------
+
+@api_view(['POST'])
+def getresult(request):
+    module_path = "E:/ML/universal_sentence_encoder_1"
+    if request.method == 'POST':
+        #1 preproceessing
+        sentences = request.data
+        sentences = [sentences]
+        sentences = PreprocessingLayer(sentences)
+
+        #2 predict
+
+    #     module_path = "E:/ML/universal_sentence_encoder_1"
+    #     # Load the saved model
+    #     embed = tf.saved_model.load(module_path)
+    #     sentence_encoder_layer=hub.KerasLayer(
+    #         "https://tfhub.dev/google/universal-sentence-encoder/4",
+    #         input_shape=[],
+    #         dtype=tf.string,
+    #         name="USE"
+    #     )
+
+    #     custom_objects = {'KerasLayer': hub.KerasLayer}
+    #     model = tf.keras.models.load_model("model_2(72% ts).h5", custom_objects=custom_objects)
+
+    #     CLASSES=['POCSO-2012', 'IPC-509', 'IPC-504', 'IPC-354', 'IPC-354A', 'IPC-498A',
+    #    'IPC-302', 'IPC-376', 'DPA', 'DVA', 'IPC-34', 'IPC 307', 'IPC 313',
+    #    'IPC 109', 'IPC 324', 'IPC 326', 'IPC 323']
+    
+        # predict function call
+        result = predict(sentences, model, CLASSES)
+
+        predict = []
+        # Assuming laws is your model and lawsSerializer is the serializer for it
+        for code in result:
+            m = laws.objects.get(code=code)
+            serialized_data = lawsSerailizer(m).data
+            predict.append(serialized_data)
+        
+        return Response(predict)
+        
+def predict(case, model, classes):
+    # case = [case]
+    # case = PreprocessingLayer(case)
+    print(case)
+    pred = model.predict(case)[0]
+    top5_indices = np.argsort(pred)[::-1][:5]
+    top5_indices = top5_indices.astype(int)  # Ensure top5_indices is of type int
+    top5_classes = [classes[i] for i in top5_indices]
+    top5_probabilities = pred[top5_indices]
+    pred= []
+    for i in range(5):
+        print(f"{i + 1}. Class: {top5_classes[i]}, Probability: {top5_probabilities[i]:.4f}") 
+        pred.append(top5_classes[i])
+    return pred
+
+
+# user helper function
+def lowercase(sentences):
+    for i in range(len(sentences)):
+        sentences[i] = sentences[i].lower()
+    return sentences
+
+def remove_punctuations(sentences):
+    cleaned_sentences= []
+    for sent in sentences :
+        words = word_tokenize(sent)
+        word_without_punt = [word for word in words if word not in string.punctuation]
+        sentence_without_punct = ' '.join(word_without_punt)
+        cleaned_sentences.append(sentence_without_punct)
+
+    return cleaned_sentences
+
+def remove_stopwords(sentences):
+    stop_words = set(stopwords.words('english'))
+    filtered_sentences = []
+    for sentence in sentences:
+        words = word_tokenize(sentence)
+        words_without_stop = [word for word in words if word.lower() not in stop_words]
+        sent_without_stop = ' '.join(words_without_stop)
+        filtered_sentences.append(sent_without_stop)
+
+    return filtered_sentences
+
+def PreprocessingLayer(sentences):
+    sentences = lowercase(sentences)
+    sentences = remove_punctuations(sentences)
+    sentences = remove_stopwords(sentences)
+
+    return sentences
+
+ 
